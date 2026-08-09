7936c52a6d936d7daac6598c1989a713a5d94f06
25f1002767 <25f1002767@ds.study.iitm.ac.in>
2026-08-09 20:17:40 +0530

    Add Render deployment configuration

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ app.config.from_object(Config)
 # ==========================
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 # ==========================
 # Register Blueprints
@@ -113,5 +115,3 @@ if __name__ == "__main__":
     app.run(
         debug=True
     )
-
-    
